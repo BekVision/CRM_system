d4aa7c879d92d11c7eee7c92397eb78b85ccb7bc
BekVision <asad@student.pdp.university>
2025-05-25 16:29:08 +0500

    all add

--- clothes/settings.py
+++ clothes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(umag!90#mi*323po)4-oqy-gjhnes^%6nmxna0*ur9@h#v@*_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["164.90.172.45", "localhost", "bek85.me", "127.0.0.1:8005"]
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
